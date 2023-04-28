ea6f30899c74c55d678a5ca2afc27682d54c7f6c
vijay vignesh <vijay.vignesh@research.iiit.ac.in>
2023-04-29 02:39:00 +0530

    inital commit

---
+++ cp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+wget https://s3.amazonaws.com/openneuro/ds000113b/ds000113b_R2.0.1/compressed/ds000113b_R2.0.1_sub04-06.zip
+
+wget https://s3.amazonaws.com/openneuro/ds000113b/ds000113b_R2.0.1/compressed/ds000113b_R2.0.1_sub07-09.zip
+
+wget https://s3.amazonaws.com/openneuro/ds000113b/ds000113b_R2.0.1/compressed/ds000113b_R2.0.1_sub10-12.zip
+
+wget https://s3.amazonaws.com/openneuro/ds000113b/ds000113b_R2.0.1/compressed/ds000113b_R2.0.1_sub13-16.zip
+
+wget https://s3.amazonaws.com/openneuro/ds000113b/ds000113b_R2.0.1/compressed/ds000113b_R2.0.1_sub17-20.zip
+
+
